e072abf85d871b8a0dab6fca12163e54ed253e11
Luis Pinto <dapin46@hotmail.com>
2023-06-25 09:09:04 -0500

    Audio red added succesfully

--- src/sounds/english/colors/soundColors.js
+++ src/sounds/english/colors/soundColors.js
@@ -1,13 +1,14 @@
 import black from "./Black.wav";
 import blue from "./Blue.wav";
 import green from "./Green.wav";
+import red from "./Red.wav";
 import orange from "./Orange.wav";
 import purple from "./Purple.wav";
 import white from "./White.wav";
 import yellow from "./Yellow.wav";
 //import yikanka from "./Yikanka.wav";
 
-const colors = [white, yellow, blue, orange, orange, purple, black, green];
+const colors = [white, yellow, blue, red, orange, purple, black, green];
 
 const soundColors = [];
 for (let i = 0; i <= colors.length; i++) {
